f247919b66bb114b6e461488238504a99e5a20bc
Nick L. <lesseosnick@gmail.com>
2026-07-26 13:30:50 -0700

    fix: meter Black Forest Labs image generation cost in the image cost dispatch

--- litellm/litellm_core_utils/llm_cost_calc/utils.py
+++ litellm/litellm_core_utils/llm_cost_calc/utils.py
@@ -1174,6 +1174,15 @@ class CostCalculatorUtils:
                 model=model,
                 image_response=completion_response,
             )
+        elif custom_llm_provider == litellm.LlmProviders.BLACK_FOREST_LABS.value:
+            from litellm.llms.black_forest_labs.cost_calculator import (
+                image_cost_calculator as bfl_image_cost_calculator,
+            )
+
+            return bfl_image_cost_calculator(
+                model=model,
+                image_response=completion_response,
+            )
         elif custom_llm_provider == litellm.LlmProviders.RUNWAYML.value:
             from litellm.llms.runwayml.cost_calculator import (
                 cost_calculator as runwayml_image_cost_calculator,
